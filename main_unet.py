9eb34e127b659f0e153e925542464f3a14e72aa6
justlab11 <58867890+justlab11@users.noreply.github.com>
2025-01-19 06:32:41 -0500

    tweaked main and added a new model folder for unet stuff

--- main_unet.py
+++ main_unet.py
@@ -105,74 +105,75 @@ val_loader: DataLoader = DataLoader(
     batch_size=8
 )
 
-if not os.path.exists("models"):
-    os.mkdir("models")
+if not os.path.exists("models_unet"):
+    os.mkdir("models_unet")
 
-num_contrast_epochs = 200
-temp_range = np.linspace(0.05, .15, 3)
-best_val_loss = 1000*np.ones(len(temp_range))
+if not os.path.exists(f"models_unet/contrast_body_plain+skips.pt"):
+    num_contrast_epochs = 200
+    temp_range = np.linspace(0.05, .15, 3)
+    best_val_loss = 1000*np.ones(len(temp_range))
 
-for i, temp in enumerate(temp_range):
-    model = TinyCNN_Headless()
-    proj_head = torch.nn.Linear(32, 128)
+    for i, temp in enumerate(temp_range):
+        model = TinyCNN_Headless()
+        proj_head = torch.nn.Linear(32, 128)
 
-    model.to(DEVICE)
-    proj_head.to(DEVICE)
+        model.to(DEVICE)
+        proj_head.to(DEVICE)
 
-    contrast_optimizer = optim.Adam(
-        list(model.parameters()) + list(proj_head.parameters()),
-        lr=0.001, 
-        weight_decay=1e-5
-    )
-
-    contrast_early_stopper = EarlyStopper(
-        patience=10,
-        min_delta=0
-    )
-
-    for epoch in range(num_contrast_epochs):
-        train_loss = contrastive_run(
-            model=model,
-            proj_head=proj_head,
-            optimizer=contrast_optimizer,
-            dataloader=train_loader,
-            device=DEVICE,
-            temperature=temp
+        contrast_optimizer = optim.Adam(
+            list(model.parameters()) + list(proj_head.parameters()),
+            lr=0.001, 
+            weight_decay=1e-5
         )
 
-        val_loss = contrastive_run(
-            model=model,
-            proj_head=proj_head,
-            optimizer=contrast_optimizer,
-            dataloader=val_loader,
-            device=DEVICE,
-            train=False,
-            temperature=temp
+        contrast_early_stopper = EarlyStopper(
+            patience=10,
+            min_delta=0
         )
 
-        if val_loss < best_val_loss[i]:
-            best_val_loss[i] = val_loss
-            torch.save(model.state_dict(), f"models/contrast_body_plain+skips_{round(temp, 2)}.pt")
-            torch.save(proj_head.state_dict(), f"models/contrast_proj_plain+skips_{round(temp, 2)}.pt")
+        for epoch in range(num_contrast_epochs):
+            train_loss = contrastive_run(
+                model=model,
+                proj_head=proj_head,
+                optimizer=contrast_optimizer,
+                dataloader=train_loader,
+                device=DEVICE,
+                temperature=temp
+            )
 
-        if contrast_early_stopper(val_loss):
-            print(f"\nBest Val Loss ({round(temp, 2)}):", best_val_loss[i])
-            break
+            val_loss = contrastive_run(
+                model=model,
+                proj_head=proj_head,
+                optimizer=contrast_optimizer,
+                dataloader=val_loader,
+                device=DEVICE,
+                train=False,
+                temperature=temp
+            )
 
-        if (epoch+1 == num_contrast_epochs):
-            print(f"\nBest Val Loss ({round(temp, 2)}):", best_val_loss[i])
+            if val_loss < best_val_loss[i]:
+                best_val_loss[i] = val_loss
+                torch.save(model.state_dict(), f"models_unet/contrast_body_plain+skips_{round(temp, 2)}.pt")
+                torch.save(proj_head.state_dict(), f"models_unet/contrast_proj_plain+skips_{round(temp, 2)}.pt")
 
-best_temp = round(temp_range[np.argmin(best_val_loss)], 2)
-print(f"Best temp: {best_temp}")
+            if contrast_early_stopper(val_loss):
+                print(f"\nBest Val Loss ({round(temp, 2)}):", best_val_loss[i])
+                break
 
-# rename the best temp to be the best
-os.rename(f"models/contrast_body_plain+skips_{best_temp}.pt", f"models/contrast_body_plain+skips.pt")
+            if (epoch+1 == num_contrast_epochs):
+                print(f"\nBest Val Loss ({round(temp, 2)}):", best_val_loss[i])
+
+    best_temp = round(temp_range[np.argmin(best_val_loss)], 2)
+    print(f"Best temp: {best_temp}")
+
+    # rename the best temp to be the best
+    os.rename(f"models_unet/contrast_body_plain+skips_{best_temp}.pt", f"models_unet/contrast_body_plain+skips.pt")
 
 # train contrastive learning classifier
 num_class_epochs = 20
 
 contrast_body = TinyCNN_Headless()
-contrast_body.load_state_dict(torch.load(f"models/contrast_body_plain+skips.pt", weights_only=True))
+contrast_body.load_state_dict(torch.load(f"models_unet/contrast_body_plain+skips.pt", weights_only=True))
 
 class_head = TinyCNN_Head()
 
@@ -220,7 +221,7 @@ for epoch in range(num_class_epochs):
     if val_loss < contrast_best["val_loss"]:
         contrast_best["val_loss"] = val_loss
         contrast_best["val_acc"] = val_acc
-        torch.save(wrapped_model.state_dict(), "models/contrast_class_plain+skips.pt")
+        torch.save(wrapped_model.state_dict(), "models_unet/contrast_class_plain+skips.pt")
 
     if contrast_early_stopper(val_loss):
         print("\nBest Val Loss:", contrast_best["val_loss"])
@@ -233,11 +234,12 @@ for epoch in range(num_class_epochs):
 
 # training the unet model
 contrast_body = TinyCNN_Headless()
-contrast_body.load_state_dict(torch.load(f"models/contrast_body_plain+skips.pt", weights_only=True))
+contrast_body.load_state_dict(torch.load(f"models_unet/contrast_body_plain+skips.pt", weights_only=True))
 contrast_body.eval()
 
 unet_model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
     in_channels=3, out_channels=3, init_features=32, pretrained=False)
+
 
 unet_optimizer = optim.Adam(
     unet_model.parameters(),
@@ -250,18 +252,20 @@ unet_val_loss = 1e7
 
 for epoch in range(num_epochs_warm_start):
     train_loss = unet_run(
-        unet_model=unet_run,
+        unet_model=unet_model,
         classifier=contrast_body,
         optimizer=unet_optimizer,
         dataloader=train_loader,
+        device=DEVICE,
         train=True
     )
 
     val_loss = unet_run(
-        unet_model=unet_run,
+        unet_model=unet_model,
         classifier=contrast_body,
         optimizer=unet_optimizer,
         dataloader=val_loader,
+        device=DEVICE,
         train=False
     )
 
