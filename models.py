a308bdac83bc6e26257167a153fd624557d8d0f2
justlab11 <justlab11@gmail.com>
2025-01-31 02:26:36 -0500

    removed all CNNs due to redundancy, except for tinycnn due to previous model's creation methods

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
 
 class DynamicCNN(nn.Module):
     def __init__(self, input_shape:tuple, num_filters:list=[32, 32], kernel_size:list=[3], stride:list=[1], padding:list=[1],
@@ -114,81 +113,6 @@ class DynamicCNN(nn.Module):
         
         return layer_outputs
 
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 3 * 3, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.dropout = nn.Dropout(0.5)
-
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = self.pool(torch.relu(self.conv3(x)))
-        x = x.view(-1, 64 * 3 * 3)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
-    
-class CNN_Headless(nn.Module):
-    def __init__(self):
-        super(CNN_Headless, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 3 * 3, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.dropout = nn.Dropout(0.5)
-
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = self.pool(torch.relu(self.conv3(x)))
-        x = x.view(-1, 64 * 3 * 3)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)
-        return x
-
-class SmallCNN(nn.Module):
-    def __init__(self):
-        super(SmallCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 64)
-        self.fc2 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-    
-class SmallCNN_Headless(nn.Module):
-    def __init__(self):
-        super(SmallCNN_Headless, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 64)
-        self.fc2 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
-        x = torch.relu(self.fc1(x))
-        return x
-
 class TinyCNN(nn.Module):
     def __init__(self):
         super(TinyCNN, self).__init__()
@@ -200,14 +124,6 @@ class TinyCNN(nn.Module):
         self.fc3 = nn.Linear(32, 10)
 
     def forward(self, x):
-        # x = self.pool(torch.relu(self.conv1(x)))
-        # x = self.pool(torch.relu(self.conv2(x)))
-        # x = x.view(-1, 16 * 7 * 7)
-        # x = torch.relu(self.fc1(x))
-        # x = torch.relu(self.fc2(x))
-        # x = self.fc3(x)
-        # return x
-
         layer_outputs = []
         
         # Conv1 layer
@@ -270,12 +186,6 @@ class TinyCNN_Headless(nn.Module):
         self.fc1 = nn.Linear(16 * 8 * 8, 32)
 
     def forward(self, x):
-        # x = self.pool(torch.relu(self.conv1(x)))
-        # x = self.pool(torch.relu(self.conv2(x)))
-        # x = x.view(-1, 16 * 7 * 7)
-        # x = torch.relu(self.fc1(x))
-        # return x
-
         layer_outputs = []
         
         # Conv1 layer
@@ -335,21 +245,6 @@ class TinyCNN_Head(nn.Module):
         layer_outputs.append(fc2_layer)
         
         return layer_outputs
-
-    
-class Resnet18(nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super(TinyCNN, self).__init__()
-        if pretrained:
-            self.model = resnet18(weights="DEFAULT")
-        else:
-            self.model = resnet18(weights=None)
-
-        num_ftrs = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        return self.model(x)
     
 class WrapperModelTrainHead(nn.Module):
     def __init__(self, body, head):
